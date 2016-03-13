dc0e559da58ee71d66b6146233eadd8728f6cbd9
Tae <taehyun.lim@gmail.com>
2016-03-13 10:11:43 +0000

    End of lesson 5 - setup models and database, migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Nomstae::Application.routes.draw do
+  root 'places#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
